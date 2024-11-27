fa376ed175125e40f67b6ad7dcfc473c2a141433
Vitaly Trakhtenberg <webdev@tca.by>
2024-11-27 22:21:45 +0200

    Developed all operations, tested in cooperations to the server

--- src/main/java/telran/employees/client/Settings.java
+++ src/main/java/telran/employees/client/Settings.java
@@ -2,8 +2,8 @@ package telran.employees.client;
 
 public class Settings
 {
-    public static final int PORT_FROM = 3000;
-    public static final int PORT_TO = 3010;
+    public static final int PORT_FROM = 3498;
+    public static final int PORT_TO = 3510;
     public static final String host = "localhost";
 
 }
